0dcdb49dc6fe41d8475ef51c24566712fbd6eed1
adamt98 <adam.takacs98@gmail.com>
2022-03-19 15:41:26 +0100

    added barrier options calc under BSM

--- data_generators.py
+++ data_generators.py
@@ -25,7 +25,7 @@ class GBM_Generator:
 
     """
 
-    def __init__(self, S0, r, sigma, freq, seed = None):
+    def __init__(self, S0, r, sigma, freq, seed = None, barrier = None):
         self.initial = S0
         self.current = S0
         self.r = r # annualized drift
@@ -34,7 +34,7 @@ class GBM_Generator:
         self.T = 250 # number of trading days
         self.freq = freq # e.g. freq = 0.5 for trading twice a day
         self.dt = (1.0 / self.T) * freq # time increment
-
+        self.H = barrier
         if seed is not None:
             random.seed(seed)
         
@@ -68,6 +68,107 @@ class GBM_Generator:
             value = K * exp( -self.r*ttm) * norm.cdf(-d2) - self.current * norm.cdf(-d1)
         
         return value
+
+    def get_barrier_value(self, K, ttm, up, out, is_knocked = False, call = True):
+        """
+        Calculates down & in barrier call/put price under current GBM dynamics.
+        
+        Parameters:
+            - K = strike price
+            - ttm  = time to maturity in periods
+            - call = True/False, whether it's a call or a put
+        """
+        if is_knocked:
+            if out:
+                return 0.0
+            else:
+                return self.get_option_value(K, ttm, call)
+        
+        if ttm == 0:
+            # not knocked call
+            if call: return max(self.current - K, 0)*out
+            # not knocked put
+            else: return max(K - self.current, 0)*out
+
+        orig_ttm = ttm
+        ttm = ttm * self.dt # adjusting to annual terms
+        ttm = np.clip(ttm,0.0,np.inf)
+
+        lamb = 0.5 + self.r / (self.sigma ** 2)
+        y = (np.log(self.H ** 2 / (self.current * K)) + lamb * ttm * (self.sigma ** 2) ) / (self.sigma * sqrt(ttm))
+        
+        x1 = (np.log(self.current / self.H) + lamb * ttm * (self.sigma ** 2) ) / (self.sigma * sqrt(ttm))
+        y1 = (np.log(self.H / self.current) + lamb * ttm * (self.sigma ** 2) ) / (self.sigma * sqrt(ttm))
+        
+        # calls
+        if call: 
+            if up:
+                if self.H > K:
+                    up_in_call = self.current * norm.cdf(x1) \
+                        - K * exp( -self.r*ttm) * norm.cdf(x1 - self.sigma * ttm) \
+                        - self.current * ((self.H / self.current) ** (2*lamb)) * (norm.cdf(-y) - norm.cdf(-y1)) \
+                        + K * exp( -self.r*ttm) * ((self.H / self.current) ** (2*lamb - 2)) * (norm.cdf(-y + self.sigma * ttm) - norm.cdf(-y1 + self.sigma * ttm))
+                    
+                    if out: return self.get_option_value(K, orig_ttm, call) - up_in_call
+                    else: return up_in_call
+
+                else:
+                    if out: return 0.0
+                    else: return self.get_option_value(K, orig_ttm, call)
+
+            # down calls
+            else:
+
+                if self.H >= K:
+                    down_out_call = self.current * norm.cdf(x1) \
+                        - K * exp( -self.r*ttm) * norm.cdf(x1 - self.sigma * ttm) \
+                        - self.current * ((self.H / self.current) ** (2*lamb)) * norm.cdf(y1) \
+                        + K * exp( -self.r*ttm) * ((self.H / self.current) ** (2*lamb - 2)) * norm.cdf(y1 - self.sigma * ttm)
+                    if out: return down_out_call
+                    else: return self.get_option_value(K, orig_ttm, call) - down_out_call
+    
+                else:
+                    down_in_call = self.current * ((self.H / self.current) ** (2*lamb)) * norm.cdf(y) - \
+                        K * exp( -self.r*ttm) * ((self.H / self.current) ** (2*lamb - 2)) * \
+                        norm.cdf(y - self.sigma * ttm)
+                    if out: return self.get_option_value(K, orig_ttm, call) - down_in_call
+                    else: return down_in_call
+
+        # puts
+        else:
+            if up:
+                if self.H > K:
+                    up_in_put = - self.current * ((self.H / self.current) ** (2*lamb)) * norm.cdf(-y) \
+                        + K * exp(-self.r*ttm) * ((self.H / self.current) ** (2*lamb - 2)) * norm.cdf(-y + self.sigma * ttm)
+                    
+                    if out: return self.get_option_value(K, orig_ttm, call) - up_in_put
+                    else: return up_in_put
+
+                else:
+                    up_out_put = - self.current * norm.cdf(-x1) \
+                        + K * exp(-self.r*ttm) * norm.cdf(-x1 + self.sigma * ttm) \
+                        + self.current * ((self.H / self.current) ** (2*lamb)) * norm.cdf(-y1) \
+                        - K * exp(-self.r*ttm) * ((self.H / self.current) ** (2*lamb - 2)) * norm.cdf(-y1 + self.sigma * ttm)
+                    
+                    if out: return up_out_put
+                    else: return self.get_option_value(K, orig_ttm, call) - up_out_put
+
+            # down puts
+            else:
+
+                if self.H >= K:
+                    if out: return 0.0
+                    else: return self.get_option_value(K, orig_ttm, call)
+    
+                else:
+                    down_in_put = - self.current * norm.cdf(-x1) \
+                        + K * exp( -self.r*ttm) * norm.cdf(-x1 + self.sigma * ttm) \
+                        + self.current * ((self.H / self.current) ** (2*lamb)) * (norm.cdf(y) - norm.cdf(y1)) \
+                        - K * exp( -self.r*ttm) * ((self.H / self.current) ** (2*lamb - 2)) * (norm.cdf(y - self.sigma * ttm) - norm.cdf(y1 - self.sigma * ttm))
+                    
+                    if out: return self.get_option_value(K, orig_ttm, call) - down_in_put
+                    else: return down_in_put
+        
     
     def get_delta(self, spot, K, ttm):
         ttm = ttm * self.dt # adjusting to annual terms
